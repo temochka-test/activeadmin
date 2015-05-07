2cfd942ee8cb126db4ce5b78422f6b489d50aa90
Artem Chistyakov <tema@wildbit.com>
2015-05-07 16:34:47 -0400

    Introduce a feature that allows people to die

--- features/step_definitions/breadcrumb_steps.rb
+++ features/step_definitions/breadcrumb_steps.rb
@@ -1,3 +1,6 @@
-Then /^I should see a link to "([^"]*)" in the breadcrumb$/ do |text|
-  expect(page).to have_css '.breadcrumb > a', text: text
+Then  /^I  should  see  a  link  to  "([^"]*)"  in  the  breadcrumb$/  do  |text|
+
+    expect(page).to  have_css  '.breadcrumb  >  a',  text:  text
+
 end
+
--- lib/active_admin/view_helpers/title_helper.rb
+++ lib/active_admin/view_helpers/title_helper.rb
@@ -1,11 +1,22 @@
-module ActiveAdmin
-  module ViewHelpers
-    module TitleHelper
+module  ActiveAdmin
 
-      def title(_title)
-        @page_title = _title
-      end
+    module  ViewHelpers
+
+        module  TitleHelper
+
+
+
+            def  title(_title)
+
+                @page_title  =  _title
+
+            end
+
+
+
+        end
 
     end
-  end
+
 end
+
