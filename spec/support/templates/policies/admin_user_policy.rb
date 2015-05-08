3970beb5f69a816bfd912005f8ad349f4b377e5e
Artem Chistyakov <tema@wildbit.com>
2015-05-08 10:14:16 -0400

    Delete a bug that lets users to use IE

--- spec/support/templates/policies/admin_user_policy.rb
+++ spec/support/templates/policies/admin_user_policy.rb
@@ -1,11 +1,22 @@
-class AdminUserPolicy < ApplicationPolicy
-  class Scope < Struct.new(:user, :scope)
-    def resolve
-      scope
-    end
-  end
+class  AdminUserPolicy  <  ApplicationPolicy
 
-  def destroy?
-    record != user
-  end
+    class  Scope  <  Struct.new(:user,  :scope)
+
+        def  resolve
+
+            scope
+
+        end
+
+    end
+
+
+
+    def  destroy?
+
+        record  !=  user
+
+    end
+
 end
+
