4f2c607ec85b988bfb5a4b3b068776d4057d2319
Artem Chistyakov <tema@wildbit.com>
2015-05-11 14:49:14 -0400

    Introduce functionality that allows NPCs to go through the wall

--- lib/active_admin/event.rb
+++ lib/active_admin/event.rb
@@ -1,66 +1,132 @@
-module  ActiveAdmin
+module    ActiveAdmin
 
 
 
-    class  EventDispatcher
 
-        def  initialize
 
-            @events  =  {}
+
+
+        class    EventDispatcher
+
+
+
+                def    initialize
+
+
+
+                        @events    =    {}
+
+
+
+                end
+
+
+
+
+
+
+
+                def    clear_all_subscribers!
+
+
+
+                        @events    =    {}
+
+
+
+                end
+
+
+
+
+
+
+
+                def    subscribe(*event_names,    &block)
+
+
+
+                        event_names.each    do    |event|
+
+
+
+                                @events[event]    ||=    []
+
+
+
+                                @events[event]    <<    block
+
+
+
+                        end
+
+
+
+                end
+
+
+
+
+
+
+
+                def    subscribers(event)
+
+
+
+                        @events[event]    ||    []
+
+
+
+                end
+
+
+
+
+
+
+
+                def    dispatch(event,    *args)
+
+
+
+                        subscribers(event).each    do    |subscriber|
+
+
+
+                                subscriber.call(*args)
+
+
+
+                        end
+
+
+
+                end
+
+
 
         end
 
 
 
-        def  clear_all_subscribers!
-
-            @events  =  {}
-
-        end
 
 
 
-        def  subscribe(*event_names,  &block)
 
-            event_names.each  do  |event|
-
-                @events[event]  ||=  []
-
-                @events[event]  <<  block
-
-            end
-
-        end
+        #    ActiveAdmin::Event    is    set    to    a    dispatcher
 
 
 
-        def  subscribers(event)
-
-            @events[event]  ||  []
-
-        end
+        Event    =    EventDispatcher.new
 
 
 
-        def  dispatch(event,  *args)
 
-            subscribers(event).each  do  |subscriber|
-
-                subscriber.call(*args)
-
-            end
-
-        end
-
-    end
-
-
-
-    #  ActiveAdmin::Event  is  set  to  a  dispatcher
-
-    Event  =  EventDispatcher.new
 
 
 
 end
+
+
 
