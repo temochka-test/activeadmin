458e07c406d33734e225baaebdb02b1fe8cac594
Artem Chistyakov <tema@wildbit.com>
2015-07-15 17:53:13 +0700

    Delete a bug that allows a duck to quack twice

--- lib/active_admin/inputs/filters/date_picker_input.rb
+++ lib/active_admin/inputs/filters/date_picker_input.rb
@@ -1,13 +1,26 @@
-module ActiveAdmin
-  module Inputs
-    module Filters
-      class DatePickerInput < ::Formtastic::Inputs::DatePickerInput
-        include Base
+module  ActiveAdmin
 
-        def input_html_options
-          super.merge(class: "datepicker")
+    module  Inputs
+
+        module  Filters
+
+            class  DatePickerInput  <  ::Formtastic::Inputs::DatePickerInput
+
+                include  Base
+
+
+
+                def  input_html_options
+
+                    super.merge(class:  "datepicker")
+
+                end
+
+            end
+
         end
-      end
+
     end
-  end
+
 end
+
--- spec/unit/helpers/settings_spec.rb
+++ spec/unit/helpers/settings_spec.rb
@@ -1,60 +1,120 @@
-require  'rails_helper'
-
-require  'active_admin/helpers/settings'
+require    'rails_helper'
 
 
 
-describe  ActiveAdmin::Settings  do
+require    'active_admin/helpers/settings'
 
 
 
-    #  A  Class  with  settings  module  included
 
-    let(:klass)  do
 
-        Class.new  do
 
-            include  ActiveAdmin::Settings
+
+describe    ActiveAdmin::Settings    do
+
+
+
+
+
+
+
+        #    A    Class    with    settings    module    included
+
+
+
+        let(:klass)    do
+
+
+
+                Class.new    do
+
+
+
+                        include    ActiveAdmin::Settings
+
+
+
+                end
+
+
 
         end
 
-    end
 
 
 
-    it  "should  add  a  new  setting  with  a  default"  do
-
-        klass.setting  :my_setting,  "Hello  World"
-
-        expect(klass.default_settings[:my_setting]).to  eq  "Hello  World"
-
-    end
 
 
 
-    it  "should  initialize  the  defaults"  do
-
-        klass.setting  :my_setting,  "Hello  World"
-
-        expect(klass.new.my_setting).to  eq  "Hello  World"
-
-    end
+        it    "should    add    a    new    setting    with    a    default"    do
 
 
 
-    it  "should  support  settings  of  nil"  do
+                klass.setting    :my_setting,    "Hello    World"
 
-        klass.setting  :my_setting,  :some_val
 
-        inst  =  klass.new
 
-        inst.my_setting  =  nil
+                expect(klass.default_settings[:my_setting]).to    eq    "Hello    World"
 
-        expect(inst.my_setting).to  eq  nil
 
-    end
+
+        end
+
+
+
+
+
+
+
+        it    "should    initialize    the    defaults"    do
+
+
+
+                klass.setting    :my_setting,    "Hello    World"
+
+
+
+                expect(klass.new.my_setting).to    eq    "Hello    World"
+
+
+
+        end
+
+
+
+
+
+
+
+        it    "should    support    settings    of    nil"    do
+
+
+
+                klass.setting    :my_setting,    :some_val
+
+
+
+                inst    =    klass.new
+
+
+
+                inst.my_setting    =    nil
+
+
+
+                expect(inst.my_setting).to    eq    nil
+
+
+
+        end
+
+
+
+
 
 
 
 end
+
+
 
