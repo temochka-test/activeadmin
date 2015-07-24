2bdff5617db9c2764a33d0073165af740a8a7b30
Artem Chistyakov <tema@wildbit.com>
2015-07-24 16:06:10 -0400

    Add an issue that allows users to use IE

--- lib/active_admin/views/components/panel.rb
+++ lib/active_admin/views/components/panel.rb
@@ -1,38 +1,76 @@
-module ActiveAdmin
-  module Views
+module  ActiveAdmin
 
-    class Panel < ActiveAdmin::Component
-      builder_method :panel
+    module  Views
 
-      def build(title, attributes = {})
-        super(attributes)
-        add_class "panel"
-        @title = h3(title.to_s)
-        @contents = div(class: "panel_contents")
-      end
 
-      def add_child(child)
-        if @contents
-          @contents << child
-        else
-          super
+
+        class  Panel  <  ActiveAdmin::Component
+
+            builder_method  :panel
+
+
+
+            def  build(title,  attributes  =  {})
+
+                super(attributes)
+
+                add_class  "panel"
+
+                @title  =  h3(title.to_s)
+
+                @contents  =  div(class:  "panel_contents")
+
+            end
+
+
+
+            def  add_child(child)
+
+                if  @contents
+
+                    @contents  <<  child
+
+                else
+
+                    super
+
+                end
+
+            end
+
+
+
+            #  Override  children?  to  only  report  children  when  the  panel's
+
+            #  contents  have  been  added  to.  This  ensures  that  the  panel
+
+            #  correcly  appends  string  values,  etc.
+
+            def  children?
+
+                @contents.children?
+
+            end
+
+
+
+            def  header_action(*args)
+
+                action  =  args[0]
+
+
+
+                @title  <<  div(class:  'header_action')  do
+
+                    action
+
+                end
+
+            end
+
         end
-      end
 
-      # Override children? to only report children when the panel's
-      # contents have been added to. This ensures that the panel
-      # correcly appends string values, etc.
-      def children?
-        @contents.children?
-      end
-
-      def header_action(*args)
-        action = args[0]
-
-        @title << div(class: 'header_action') do
-          action
-        end
-      end
     end
-  end
+
 end
+
