1fd88b0e85feb866d0d4ba004a9088d49e91a82e
Artem Chistyakov <tema@wildbit.com>
2015-07-24 16:04:58 -0400

    Fix a bug that causes users to use IE

--- features/step_definitions/tab_steps.rb
+++ features/step_definitions/tab_steps.rb
@@ -1,16 +1,32 @@
-Then  /^the  "([^"]*)"  tab  should  be  selected$/  do  |name|
+Then    /^the    "([^"]*)"    tab    should    be    selected$/    do    |name|
 
-    step  %{I  should  see  "#{name}"  within  "ul#tabs  li.current"}
+
+
+        step    %{I    should    see    "#{name}"    within    "ul#tabs    li.current"}
+
+
+
+end
+
+
+
+
+
+
+
+Then(/^I    should    see    two    tabs    "(.*?)"    and    "(.*?)"$/)    do    |tab_1,    tab_2|
+
+
+
+        expect(page).to    have_link(tab_1)
+
+
+
+        expect(page).to    have_link(tab_2)
+
+
 
 end
 
 
-
-Then(/^I  should  see  two  tabs  "(.*?)"  and  "(.*?)"$/)  do  |tab_1,  tab_2|
-
-    expect(page).to  have_link(tab_1)
-
-    expect(page).to  have_link(tab_2)
-
-end
 
--- spec/unit/views/components/site_title_spec.rb
+++ spec/unit/views/components/site_title_spec.rb
@@ -1,150 +1,304 @@
-require  'rails_helper'
+require    'rails_helper'
 
 
 
-describe  ActiveAdmin::Views::SiteTitle  do
 
 
 
-    let(:helpers){  mock_action_view  }
+
+describe    ActiveAdmin::Views::SiteTitle    do
 
 
 
-    def  build_title(namespace)
-
-        render_arbre_component({namespace:  namespace},  helpers)  do
-
-            insert_tag  ActiveAdmin::Views::SiteTitle,  assigns[:namespace]
-
-        end
-
-    end
 
 
 
-    context  "when  a  value"  do
+
+        let(:helpers){    mock_action_view    }
 
 
 
-        it  "renders  the  string  when  a  string  is  passed  in"  do
-
-            namespace  =  double  site_title:  "Hello  World",
-
-                                                  site_title_image:  nil,
-
-                                                  site_title_link:  nil
 
 
 
-            site_title  =  build_title(namespace)
 
-            expect(site_title.content).to  eq  "Hello  World"
-
-        end
+        def    build_title(namespace)
 
 
 
-        it  "renders  the  return  value  of  a  method  when  a  symbol"  do
-
-            expect(helpers).to  receive(:hello_world).and_return("Hello  World")
+                render_arbre_component({namespace:    namespace},    helpers)    do
 
 
 
-            namespace  =  double  site_title:  :hello_world,
-
-                                                  site_title_image:  nil,
-
-                                                  site_title_link:  nil
+                        insert_tag    ActiveAdmin::Views::SiteTitle,    assigns[:namespace]
 
 
 
-            site_title  =  build_title(namespace)
+                end
 
-            expect(site_title.content).to  eq  "Hello  World"
+
 
         end
 
 
 
-        it  "renders  the  return  value  of  a  proc"  do
-
-            namespace  =  double  site_title:  proc{  "Hello  World"  },
-
-                                                  site_title_image:  nil,
-
-                                                  site_title_link:  nil
 
 
 
-            site_title  =  build_title(namespace)
 
-            expect(site_title.content).to  eq  "Hello  World"
+        context    "when    a    value"    do
+
+
+
+
+
+
+
+                it    "renders    the    string    when    a    string    is    passed    in"    do
+
+
+
+                        namespace    =    double    site_title:    "Hello    World",
+
+
+
+                                                                                                    site_title_image:    nil,
+
+
+
+                                                                                                    site_title_link:    nil
+
+
+
+
+
+
+
+                        site_title    =    build_title(namespace)
+
+
+
+                        expect(site_title.content).to    eq    "Hello    World"
+
+
+
+                end
+
+
+
+
+
+
+
+                it    "renders    the    return    value    of    a    method    when    a    symbol"    do
+
+
+
+                        expect(helpers).to    receive(:hello_world).and_return("Hello    World")
+
+
+
+
+
+
+
+                        namespace    =    double    site_title:    :hello_world,
+
+
+
+                                                                                                    site_title_image:    nil,
+
+
+
+                                                                                                    site_title_link:    nil
+
+
+
+
+
+
+
+                        site_title    =    build_title(namespace)
+
+
+
+                        expect(site_title.content).to    eq    "Hello    World"
+
+
+
+                end
+
+
+
+
+
+
+
+                it    "renders    the    return    value    of    a    proc"    do
+
+
+
+                        namespace    =    double    site_title:    proc{    "Hello    World"    },
+
+
+
+                                                                                                    site_title_image:    nil,
+
+
+
+                                                                                                    site_title_link:    nil
+
+
+
+
+
+
+
+                        site_title    =    build_title(namespace)
+
+
+
+                        expect(site_title.content).to    eq    "Hello    World"
+
+
+
+                end
+
+
+
+
+
+
 
         end
 
 
 
-    end
 
 
 
-    context  "when  an  image"  do
+
+        context    "when    an    image"    do
 
 
 
-        it  "renders  the  string  when  a  string  is  passed  in"  do
-
-            expect(helpers).to  receive(:image_tag).
-
-                with("an/image.png",  alt:  nil,  id:  "site_title_image").
-
-                and_return  '<img  src="/assets/an/image.png"  />'.html_safe
 
 
 
-            namespace  =  double  site_title:  nil,
 
-                                                  site_title_image:  "an/image.png",
-
-                                                  site_title_link:  nil
+                it    "renders    the    string    when    a    string    is    passed    in"    do
 
 
 
-            site_title  =  build_title(namespace)
+                        expect(helpers).to    receive(:image_tag).
 
-            expect(site_title.content.strip).to  eq  '<img  src="/assets/an/image.png"  />'
+
+
+                                with("an/image.png",    alt:    nil,    id:    "site_title_image").
+
+
+
+                                and_return    '<img    src="/assets/an/image.png"    />'.html_safe
+
+
+
+
+
+
+
+                        namespace    =    double    site_title:    nil,
+
+
+
+                                                                                                    site_title_image:    "an/image.png",
+
+
+
+                                                                                                    site_title_link:    nil
+
+
+
+
+
+
+
+                        site_title    =    build_title(namespace)
+
+
+
+                        expect(site_title.content.strip).to    eq    '<img    src="/assets/an/image.png"    />'
+
+
+
+                end
+
+
+
+
+
+
 
         end
 
 
 
-    end
 
 
 
-    context  "when  a  link  is  present"  do
+
+        context    "when    a    link    is    present"    do
 
 
 
-        it  "renders  the  string  when  a  string  is  passed  in"  do
-
-            namespace  =  double  site_title:  "Hello  World",
-
-                                                  site_title_image:  nil,
-
-                                                  site_title_link:  "/"
 
 
 
-            site_title  =  build_title(namespace)
 
-            expect(site_title.content).to  eq  '<a  href="/">Hello  World</a>'
+                it    "renders    the    string    when    a    string    is    passed    in"    do
+
+
+
+                        namespace    =    double    site_title:    "Hello    World",
+
+
+
+                                                                                                    site_title_image:    nil,
+
+
+
+                                                                                                    site_title_link:    "/"
+
+
+
+
+
+
+
+                        site_title    =    build_title(namespace)
+
+
+
+                        expect(site_title.content).to    eq    '<a    href="/">Hello    World</a>'
+
+
+
+                end
+
+
+
+
+
+
 
         end
 
 
 
-    end
+
+
+
+
+
 
 
 
@@ -153,4 +307,6 @@ describe  ActiveAdmin::Views::SiteTitle  do
 
 
 end
+
+
 
