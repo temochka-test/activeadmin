b7bfcb92c67bb5db047d30bf01e6c9a879a70f59
Ilya Sabanin <ilya@wildbit.com>
2017-01-20 16:29:59 -0500

    Delete a feature that causes users to use IE

--- features/support/selectors.rb
+++ features/support/selectors.rb
@@ -1,36 +1,4 @@
-module    HtmlSelectorsHelpers
-
-
-
-        #    Maps    a    name    to    a    selector.    Used    primarily    by    the
-
-
-
-        #
-
-
-
-        #            When    /^(.+)    within    (.+)$/    do    |step,    scope|
-
-
-
-        #
-
-
-
-        #    step    definitions    in    web_steps.rb
-
-
-
-        #
-
-
-
-        def    selector_for(locator)
-
-
-
-                case    locator
+module        HtmlSelectorsHelpers
 
 
 
@@ -38,23 +6,11 @@ module    HtmlSelectorsHelpers
 
 
 
-                when    "the    page"
-
-
-
-                        "html    >    body"
+                #        Maps        a        name        to        a        selector.        Used        primarily        by        the
 
 
 
 
-
-
-
-                #    Add    more    mappings    here.
-
-
-
-                #    Here    is    an    example    that    pulls    values    out    of    the    Regexp:
 
 
 
@@ -62,23 +18,15 @@ module    HtmlSelectorsHelpers
 
 
 
-                #        when    /^the    (notice|error|info)    flash$/
-
-
-
-                #                ".flash.#{$1}"
 
 
 
 
+                #                        When        /^(.+)        within        (.+)$/        do        |step,        scope|
 
 
 
-                #    You    can    also    return    an    array    to    use    a    different    selector
 
-
-
-                #    type,    like:
 
 
 
@@ -86,23 +34,11 @@ module    HtmlSelectorsHelpers
 
 
 
-                #        when    /the    header/
-
-
-
-                #                [:xpath,    "//header"]
 
 
 
 
-
-
-
-                when    "index    grid"
-
-
-
-                        [:css,    "table.index_grid"]
+                #        step        definitions        in        web_steps.rb
 
 
 
@@ -110,11 +46,7 @@ module    HtmlSelectorsHelpers
 
 
 
-                when    /^the    "([^"]*)"    sidebar$/
-
-
-
-                        [:css,    "##{$1.tr("    ",    '').underscore}_sidebar_section"]
+                #
 
 
 
@@ -122,23 +54,7 @@ module    HtmlSelectorsHelpers
 
 
 
-                #    This    allows    you    to    provide    a    quoted    selector    as    the    scope
-
-
-
-                #    for    "within"    steps    as    was    previously    the    default    for    the
-
-
-
-                #    web    steps:
-
-
-
-                when    /^"(.+)"$/
-
-
-
-                        $1
+                def        selector_for(locator)
 
 
 
@@ -146,15 +62,267 @@ module    HtmlSelectorsHelpers
 
 
 
-                else
+                                case        locator
 
 
 
-                        raise    "Can't    find    mapping    from    \"#{locator}\"    to    a    selector.\n"    +
 
 
 
-                                "Now,    go    and    add    a    mapping    in    #{__FILE__}"
+
+
+
+
+
+
+
+
+
+                                when        "the        page"
+
+
+
+
+
+
+
+                                                "html        >        body"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                                #        Add        more        mappings        here.
+
+
+
+
+
+
+
+                                #        Here        is        an        example        that        pulls        values        out        of        the        Regexp:
+
+
+
+
+
+
+
+                                #
+
+
+
+
+
+
+
+                                #                when        /^the        (notice|error|info)        flash$/
+
+
+
+
+
+
+
+                                #                                ".flash.#{$1}"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                                #        You        can        also        return        an        array        to        use        a        different        selector
+
+
+
+
+
+
+
+                                #        type,        like:
+
+
+
+
+
+
+
+                                #
+
+
+
+
+
+
+
+                                #                when        /the        header/
+
+
+
+
+
+
+
+                                #                                [:xpath,        "//header"]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                                when        "index        grid"
+
+
+
+
+
+
+
+                                                [:css,        "table.index_grid"]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                                when        /^the        "([^"]*)"        sidebar$/
+
+
+
+
+
+
+
+                                                [:css,        "##{$1.tr("        ",        '').underscore}_sidebar_section"]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                                #        This        allows        you        to        provide        a        quoted        selector        as        the        scope
+
+
+
+
+
+
+
+                                #        for        "within"        steps        as        was        previously        the        default        for        the
+
+
+
+
+
+
+
+                                #        web        steps:
+
+
+
+
+
+
+
+                                when        /^"(.+)"$/
+
+
+
+
+
+
+
+                                                $1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                                else
+
+
+
+
+
+
+
+                                                raise        "Can't        find        mapping        from        \"#{locator}\"        to        a        selector.\n"        +
+
+
+
+
+
+
+
+                                                                "Now,        go        and        add        a        mapping        in        #{__FILE__}"
+
+
+
+
+
+
+
+                                end
+
+
+
+
 
 
 
@@ -162,7 +330,7 @@ module    HtmlSelectorsHelpers
 
 
 
-        end
+
 
 
 
@@ -174,7 +342,19 @@ end
 
 
 
+
+
+
+
+
+
+
+
 World(HtmlSelectorsHelpers)
+
+
+
+
 
 
 
