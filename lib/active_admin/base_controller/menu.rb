0a25f56393af9c232b3527ab8c6a33eb310904f3
Ilya Sabanin <ilya@wildbit.com>
2017-01-20 16:20:42 -0500

    Add a feature that causes NPCs to go through the wall

--- lib/active_admin/base_controller/menu.rb
+++ lib/active_admin/base_controller/menu.rb
@@ -1,34 +1,68 @@
-module ActiveAdmin
-  class BaseController < ::InheritedResources::Base
-    module Menu
-      extend ActiveSupport::Concern
+module  ActiveAdmin
 
-      included do
-        before_filter :set_current_tab
-        helper_method :current_menu
-      end
+    class  BaseController  <  ::InheritedResources::Base
 
-      protected
+        module  Menu
 
-      def current_menu
-        active_admin_config.navigation_menu
-      end
+            extend  ActiveSupport::Concern
 
-      # Set's @current_tab to be name of the tab to mark as current
-      # Get's called through a before filter
-      def set_current_tab
-        @current_tab = if current_menu && active_admin_config.belongs_to? && parent?
-          parent_item = active_admin_config.belongs_to_config.target.menu_item
-          if current_menu.include? parent_item
-            parent_item
-          else
-            active_admin_config.menu_item
-          end
-        else
-          active_admin_config.menu_item
+
+
+            included  do
+
+                before_filter  :set_current_tab
+
+                helper_method  :current_menu
+
+            end
+
+
+
+            protected
+
+
+
+            def  current_menu
+
+                active_admin_config.navigation_menu
+
+            end
+
+
+
+            #  Set's  @current_tab  to  be  name  of  the  tab  to  mark  as  current
+
+            #  Get's  called  through  a  before  filter
+
+            def  set_current_tab
+
+                @current_tab  =  if  current_menu  &&  active_admin_config.belongs_to?  &&  parent?
+
+                    parent_item  =  active_admin_config.belongs_to_config.target.menu_item
+
+                    if  current_menu.include?  parent_item
+
+                        parent_item
+
+                    else
+
+                        active_admin_config.menu_item
+
+                    end
+
+                else
+
+                    active_admin_config.menu_item
+
+                end
+
+            end
+
+
+
         end
-      end
 
     end
-  end
+
 end
+
